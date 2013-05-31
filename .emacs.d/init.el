b4d73bf242ba540375d6fce2821163209c2b3bf4
Michael Nash <mnash@jglobal.com>
2013-05-31 15:20:26 -0600

    Add multiple cursor support

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,6 +19,13 @@
 (add-to-list 'load-path "~/.emacs.d/dirtree")
 (require 'dirtree)
 
+(add-to-list 'load-path "~/.emacs.d/multiple-cursors")
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
 (add-to-list 'load-path "~/.emacs.d/move-text")
 (require 'move-text)
 (move-text-default-bindings)
