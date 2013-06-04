17390a72cfcc855870a4330e657906875e701e96
Michael Nash <mnash@jglobal.com>
2013-06-04 13:55:03 -0600

    Add ace jump mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -143,3 +143,19 @@
 (require 'dired-details)
 (dired-details-install)
 
+(add-to-list 'load-path "~/.emacs.d/ace-jump-mode")
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+ t)
+(define-key global-map (kbd "C-0") 'ace-jump-mode)
+
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+ t)
+(eval-after-load "ace-jump-mode"
+    '(ace-jump-mode-enable-mark-sync))
+     (define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
