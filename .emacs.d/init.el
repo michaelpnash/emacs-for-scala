5f3a2ecf357a96f4de7e686393624e84c1eea605
Michael Nash <mnash@jglobal.com>
2013-06-05 19:21:15 -0600

    Indent pasted lines appropriately

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -164,3 +164,10 @@
 
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
+(dolist (command '(yank yank-pop))
+       (eval `(defadvice ,command (after indent-region activate)
+             (and (not current-prefix-arg)
+                (member major-mode '(scala-mode emacs-lisp-mode lisp-mode clojure-mode scheme-mode haskell-mode ruby-mode rspec-mode python-mode c-mode c++-mode objc-mode latex-mode plain-tex-mode))
+           (let ((mark-even-if-inactive transient-mark-mode))
+               (indent-region (region-beginning) (region-end) nil))))))
+
