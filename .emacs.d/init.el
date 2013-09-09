d3689190d774c5051d1644aa02b9285d3eeb8a86
Michael Nash <mnash@jglobal.com>
2013-09-08 19:18:49 -0600

    Add Evil, vi emulation in emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -279,3 +279,9 @@
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
 (load-theme 'solarized-dark)
+
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+
+
